9ccea3f4b18335faec89dd496a86cf4fe64dab8a
Guillaume Vincent <gvincent@oslab.fr>
2015-06-23 15:46:19 +0200

    refactoring avec une méthode récursive

--- diamond.py
+++ diamond.py
@@ -4,19 +4,16 @@
 class Diamant:
     def __init__(self, carat):
         self.carat = carat
-        self.carat_maximum = ord('A')
+        self.carat_maximal = ord('A')
 
     def tailler(self):
-        taille = ord(self.carat) - self.carat_maximum
-        diamant = ''
-        diamant += self.tailler_couronne(taille)
-        diamant += self.tailler_rondiste(taille)
-        diamant += self.tailler_cullasse(taille)
+        taille = ord(self.carat) - self.carat_maximal
+        diamant = self._façonner_diamant(taille, taille)
         return diamant
 
     def _tailler_une_tranche(self, hauteur, largeur=0):
-        arête = chr(self.carat_maximum + hauteur)
         tranche = ''.ljust(largeur)
+        arête = chr(self.carat_maximal + hauteur)
         tranche += arête
         tranche += ''.ljust(hauteur * 2 - 1)
         if hauteur > 0:
@@ -25,24 +22,19 @@ class Diamant:
         tranche += '\n'
         return tranche
 
-    def _façonner_diamant(self, hauteur, retourné=False):
-        tranches = list(range(hauteur)) if retourné else list(reversed(range(hauteur)))
+    def _façonner_diamant(self, hauteur, hauteur_origine):
         diamant = ''
-        for i in tranches:
-            diamant += self._tailler_une_tranche(hauteur=hauteur - i - 1, largeur=i + 1)
+        nouvelle_hauteur = hauteur_origine - hauteur
+        if hauteur > 0:
+            diamant += self._tailler_une_tranche(nouvelle_hauteur, hauteur)
+            diamant += self._façonner_diamant(hauteur - 1, hauteur_origine)
+        diamant += self._tailler_une_tranche(nouvelle_hauteur, hauteur)
         return diamant
-
-    def tailler_couronne(self, taille):
-        return self._façonner_diamant(taille)
-
-    def tailler_rondiste(self, taille):
-        return self._tailler_une_tranche(taille)
-
-    def tailler_cullasse(self, taille):
-        return self._façonner_diamant(taille, retourné=True)
 
 
 assert Diamant('A').tailler() == 'A\n'
 assert Diamant('B').tailler() == ' A \nB B\n A \n'
 assert Diamant('C').tailler() == '  A  \n B B \nC   C\n B B \n  A  \n'
 assert Diamant('D').tailler() == '   A   \n  B B  \n C   C \nD     D\n C   C \n  B B  \n   A   \n'
+
+print(Diamant('D').tailler())
